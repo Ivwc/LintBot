53b31e8dd4b271945f6787fbe55e5ff19fcbfbce
eddy <d36584@gmail.com>
2017-12-19 14:14:22 +0800

    no message

--- app.py
+++ app.py
@@ -632,7 +632,7 @@ def handle_message(event):
     #     return 0
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=event.message.address)
     )
 
     buttons_template = TemplateSendMessage(
